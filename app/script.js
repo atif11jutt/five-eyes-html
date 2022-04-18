ad67e9b45e34b80d1d8f15a6f0468d0b9c631da4
atif11jutt <atif11jutt@gmail.com>
2022-04-18 08:48:51 +0500

    update

--- app/script.js
+++ app/script.js
@@ -5,7 +5,10 @@ $(function () {
       items: 1,
       margin: 10,
       loop: true,
-      nav: true
+      nav: true,
+      autoplay:true,
+      autoplayTimeout: 3500,
+      autoplaySpeed: 800
     });
 
 
